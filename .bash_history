8b7518c838a8b28167f16169c215ccdf87b0401c
Tara Grimm <tararochegrimm@gmail.com>
2017-12-12 14:37:03 -0500

    Added a create, show, and new action for images controller

---
+++ .bash_history
@@ -0,0 +1,14 @@
+rails generate controller Welcome index
+rails generate controller Home index
+rails d controller WelcomeController -p
+rails d controller Welcome -p
+rails d controller Welcome
+rails routes
+rails routes
+rails generate controller Images
+rails generate controller Images
+rails routes
+rails generate model Image title:string text:text
+rails db:migrate
+rails db:create
+exit
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,6 +2,42 @@ class ImagesController < ApplicationController
     def index
     end
 
+    def show
+        #use Image.find to find the image we're interested in, passing params[:id] to get the :id paramter from the request
+        @image = Image.find(params[:id])
+
+        #the @image is an instance variable that holds a reference to the image object.  Rails will pass all isntance variables to the view
+    end
+
     def new
     end
+
+    def edit
+    end
+
+    def create
+        #initializes image model with respective attributes which are automatically mapped to the respective database columns
+        @image = Image.new(images_params)
+
+        #saves the model into the database
+        @image.save
+
+        #redirect us to the show action
+        redirect_to @image
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    #creates a private method that can be reused in the same controller
+        #private keyboard will make anything below this private! - don't put CRUD operations here.
+    private
+        def images_params
+            #these params are called Strong Parameters because they allow us to whitelist our controller parameters to prevent wrongful assignment or extra parameters be maliciously added
+            params.require(:image) .permit(:title,:text)
+        end
 end

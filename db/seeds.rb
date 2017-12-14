7d69045958c2c9306182e33e5b47c759a723d940
Tara Grimm <tararochegrimm@gmail.com>
2017-12-14 10:46:17 -0500

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+images = Image.create(
+    [
+        {
+            url:'https://img.buzzfeed.com/buzzfeed-static/static/2014-01/campaign_images/webdr06/7/14/50-reasons-why-nicolas-cage-is-the-greatest-human-1-5571-1389124720-1_big.jpg',
+            alt:'nickalt1',
+            caption:'nickcap1'
+        },
+        {
+            url:'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzMDM4Nzk2MV5BMl5BanBnXkFtZTcwNTcwNjExOQ@@._V1_UY317_CR1,0,214,317_AL_.jpg',
+            alt:'nickalt2',
+            caption:'nickcap2'
+        },
+        {
+            url:'https://nerdist.com/wp-content/uploads/2015/12/Nicolas-Cage-Con-Air-322x268.jpg',
+            alt:'nickalt3',
+            caption:'nickcap3'
+        },
+        {
+            url:'http://1.bp.blogspot.com/-4x8LvBUopUg/UP_3v-hRgcI/AAAAAAAAC90/rerm6FhEJ4I/s1600/Anthony+Lamb+-+Nick+Cage+as+Salvador+Dali.jpg',
+            alt:'nickalt4',
+            caption:'nickcap4'
+        }
+    ]
+)
